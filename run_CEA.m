16ff7ff7ed143b78d3b587fa21b586aaa6f2359e
cycyan <chingyingchen.cyc@gmail.com>
2026-07-30 09:24:18 -0700

    Add files via upload

---
+++ run_CEA.m
@@ -0,0 +1,54 @@
+% run_CEA
+
+% runs NASA CEA for a given set of variable parameters
+
+% Inputs: Pc, OF, Pamb, A_e, eps (Ae/At), Propellant Mass
+% Outputs: C*, C_tau, A_t, mdot, Thrust, Isp, Burn Time
+
+function Prop = run_CEA(Prop, params)
+
+    Engine = CEA('problem','rocket','equilibrium','fac','ac/at',params.ac_at,'o/f',Prop.OF,'p(psi)',Pc_psi,'supersonic',Prop.eps,'reactants', ...
+             'fuel','C2H5OH(L)','wt%',params.eth_ratio*100,'t(k)',params.T_fuel_inlet,'fuel','H2O(L)','wt%',(1-params.eth_ratio)*100,'t(k)', ...
+            params.T_fuel_inlet,'oxid','O2(L)','wt%',100,'t(k)',params.T_ox_inlet,'output','transport','mks','end');
+
+    % Calculate C_star
+
+    Prop.C_star = Engine.output.eql.cstar(1);
+
+     % Calculate C_tau
+
+    Prop.Ctau = Engine.output.eql.cf(end);
+
+    % Calculate Exit Pressure
+
+    Prop.Pe = Engine.output.eql.pressure(end) * 14.5038; 
+
+    % Calculate C_tau at Vaccuum
+
+    Prop.Ctau_vac = Prop.CF_pe + Prop.eps * (Prop.Pe / Pc_psi);
+
+    % Calculate throat area
+
+    Prop.A_t = params.A_e / Prop.eps;
+
+    % Calculate mdot
+
+    Prop.mdot = (Prop.A_t * Prop.Pc) / (Prop.C_star * params.cstar_eff);
+
+    % Calculate burn time
+
+    Prop.t_b = params.prop_mass / Prop.mdot;
+
+    % Calculate Ctau at set ambient pressure
+
+    Ctau_ref = Prop.CF_vac - Prop.eps * (params.P_amb / Pc_psi);
+
+    % Calculate thrust for Isp
+
+    Thrust_ref = Prop.mdot * Prop.C_star * params.cstar_eff * Ctau_ref * params.CF_eff;
+
+    % Calculate specific impulse
+
+    Prop.Isp = Thrust_ref / (Prop.mdot * 9.81);
+
+end
