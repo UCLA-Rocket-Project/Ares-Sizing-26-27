e6ba3bace8489255e9e9727c234e665bbdd3b555
cycyan <chingyingchen.cyc@gmail.com>
2026-07-30 09:25:08 -0700

    Add files via upload
    struct-returning replacement for input_parameters.py.

---
+++ input_parameters.m
@@ -0,0 +1,35 @@
+function p = input_parameters()
+
+    p.Ctau_eff = 0.98;
+
+    p.cstar_eff = 0.90;
+
+    p.P_amb = ; % psi
+
+    p.ox_density = ; % kg/m3
+
+    p.fuel_density = ; % kg/m3
+
+    p.ullage = ;
+
+    p.ox_feed_CdA = ; % m^2
+
+    p.fuel_feed_CdA = ; % m^2
+
+    p.channel_CdA = ; % m^2
+
+    p.inj_ox_CdA = ; % m^2
+
+    p.inj_f_CdA = ; % m^2
+
+    p.eth_ratio = 0.75;
+
+    p.T_fuel_inlet = ; % K
+
+    p.T_ox_inlet = ; % K
+
+    p.ac_at = ;
+
+    p.A_e = ; % m^2
+
+end
