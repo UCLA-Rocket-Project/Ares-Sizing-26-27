0ac85ab98cdcc06dbc368352b44af70f9321383d
Lorelei Rocca <loreleirocca@g.ucla.edu>
2026-07-31 11:37:19 -0700

    Update get_PV_mel.m
    Added end cap thickness calc and changed tank thickness array

--- get_PV_mel.m
+++ get_PV_mel.m
@@ -5,16 +5,16 @@
 
 % Inputs: Propellant mass, OF ratio, tank MEOP, press tank volume
 % Outputs: Struct of PV lengths and masses or -1 (COPV not possible), 
-% -2 (tanks unable to withstand pressure),-3 (vehicle too long)
+% -2 (tanks unable to withstand pressure)
 
 function PV_mel = get_PV_mel(prop_mass, OF, p_operating,press_v)
 
 % First, check if a COPV is possible (return -1 if not)
-copv_volumes = [6.8,9,12]; % L
-copv_masses= [3.9,4.9,6.9].* 2.205; % lbm
-copv_lengths = [530,560,595]/25.4; % in
+copv_volumes = [9,12]; % L
+copv_masses= [4.9,6.9].* 2.205; % lbm
+copv_lengths = [560,595]/25.4; % in
 
-if press_v > copv_volumes(length(copv_volumes)) || press_v < copv_volumes(1)
+if press_v > copv_volumes(length(copv_volumes))
     PV_mel = -1; % COPV not possible
     return
 end
@@ -37,9 +37,8 @@ FS_y = 1.25; % FOS Yield for pressure vessels
 % FF = 1.15; % Fitting factor, currently ignoring
 % E = 1; % Weld efficiency factor (can ignore)
 
-tank_thicknesses = [0.1144, 0.125, 0.1285, 0.1443, 0.162, 0.1819, ... 
-    0.2043, 0.2294, 0.25, 0.375]; % in, 
-% ^^ need to research this and make a new array for this year
+tank_thicknesses = [0.125, 0.25, 0.375]; % in, 
+% ^^ need to research available tubes to make array
 
 for t = 1:length(tank_thicknesses)
     r_i = r_o - tank_thicknesses(t);
@@ -59,7 +58,28 @@ end
 
 r_i = r_o - PV_mel.tank_wall;
 
-% Calculate tank lengths (return -3 if too long)
+% Calculate end cap thickness (crown and knuckle)
+cap_t = 0.1375; % in
+a = r_o - cap_t; % in
+knuckle_hoop = 1.5*p_operating*a/cap_t; % psi
+MOS_cap_y = S_y/(FS_y*knuckle_hoop)-1;
+
+while MOs_cap_y <= 0
+    cap_t = cap_t + 1/32; % in
+    a = r_o - cap_t; % in
+    knuckle_hoop = 1.5*p_operating*a/cap_t; % psi
+    MOS_cap_y = S_y/(FS_y*knuckle_hoop)-1;
+end
+PV_mel.cap_wall = cap_t; % in
+
+% Calculate end cap volume and mass
+a = r_o - PV_mel.cap_wall; % in
+cap_vol = (1/3*pi*a^3)+(pi*r_i^2*1.5); % in^3 (crown + tangent volume)
+cap_mass = 0.0975 * (pi/24*((r_o*2)^3-(a*2)^3) + pi*r_o*(r_o^2-r_i^2)); % lbm
+cap_mass = cap_mass * 1.3; % mark up factor based on pandora
+
+
+% Calculate barrel length and mass
 fuel_mass = prop_mass/(1+OF); % lbm
 ox_mass = OF*fuel_mass; % lbm
 fuel_density = 0.75*0.0285 + 0.25*0.036; % lb/in^3 for 75/25 ethanol/water
@@ -68,23 +88,10 @@ fuel_vol = fuel_mass/fuel_density; % in^3
 ox_vol = ox_mass/ox_density; % in^3
 ullage = 0.05;
 
-% Calculate end cap volume and mass
-% Assuming set crown thickness of 0.2 in for now
-a = r_o - 0.2; % in
-cap_vol = (1/3*pi*a^3)+(pi*r_i^2*1.5); % in^3
-cap_mass = 0.0975 * (pi/24*((r_o*2)^3-(a*2)^3) + pi*r_o*(r_o^2-r_i^2)); % lbm
-cap_mass = cap_mass * 1.3; % mark up factor based on pandora
-
-% Calculate barrel length and mass (return -3 if too long)
 fuel_length = (fuel_vol + ullage*fuel_vol - 2*cap_vol)/pi/(r_i^2); % in
 ox_length = (ox_vol + ullage*ox_vol - 2*cap_vol)/pi/(r_i^2); % in
 
-if fuel_length + ox_length > 55.5 % pandora had 47.5 in total barrel length
-    % Ruling out tanks that yield a too large L/D, still researching this
-    PV_mel = -3;
-    return
-end
-
+% Add results to struct
 PV_mel.fuel_l = fuel_length;
 PV_mel.fuel_m = 0.0975*pi*fuel_length*(r_o^2-r_i^2); % lbm
 PV_mel.ox_l = ox_length;
@@ -92,9 +99,4 @@ PV_mel.ox_m = 0.0975*pi*ox_length*(r_o^2-r_i^2); % lbm
 PV_mel.cap_m = cap_mass;
 PV_mel.pv_m = PV_mel.fuel_m + PV_mel.ox_m + 2*PV_mel.cap_m + PV_mel.copv_m;
 
-end 
-
-% need to perchance put in a new array of COPVs and definitely a new array
-% of tank thicknesses + finalize a max L/D...
-
-% might also try to incorporate iterating through end cap thicknesses
+end
