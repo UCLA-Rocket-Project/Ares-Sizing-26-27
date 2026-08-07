f4ef25870d9cac4e7d92cb58c03f219498bace10
cycyan <chingyingchen.cyc@gmail.com>
2026-08-07 16:17:25 -0700

    added hydrostatic head calcs

--- run_press.m
+++ run_press.m
@@ -11,8 +11,8 @@
 
 function [Prop, Press] = run_press(Prop, params)
 
-    mdot_fuel_SI = Prop.mdot_fuel / 2.20462; % lbm/s to kg/s
-    mdot_ox_SI = Prop.mdot_ox / 2.20462; %lbm/s to kg/s
+   % mdot_fuel_SI = Prop.mdot_fuel / 2.20462; % lbm/s to kg/s
+   %  mdot_ox_SI = Prop.mdot_ox / 2.20462; %lbm/s to kg/s
 
     % Find tank pressure needed for given chamber pressure
  
@@ -24,27 +24,66 @@ function [Prop, Press] = run_press(Prop, params)
     % fuel_dp = dP_from_CdA_mdot(fuel_CdA, mdot_fuel_SI, params.fuel_density); % psi
    %  ox_dp   = dP_from_CdA_mdot(ox_CdA, mdot_ox_SI, params.ox_density); % psi
 
+
+
+
+   % find hydrostatic pressure head at liftoff (most conservative since this adds pressure)
+
+   % find fuel and ox masses (no ullage) 
+
+    prop_mass_SI = Prop.prop_mass / 2.20462; % lbm to kg
+    fuel_mass_SI = prop_mass_SI / (Prop.OF + 1); % kg
+    ox_mass_SI   = prop_mass_SI - fuel_mass_SI; % kg
+
+   % tank geometry
+
+  r_o_SI = 4 * 0.0254; % in to m 
+  A_tank_SI = pi * r_o_SI ^2; % m^2
+
+  h_fuel_liquid = fuel_mass_SI / params.fuel_density / A_tank_SI; % m, in-tank column only
+  h_ox_liquid  = ox_mass_SI / params.ox_density / A_tank_SI;   % m
+
+  % Feed line height (tank outlet to injector face), from heritage MEL
+
+  h_fuel_feed = 28.5; 
+  h_ox_feed  = 68.95;
+  h_fuel_feed_SI = h_fuel_feed * 0.0254; % in to m
+  h_ox_feed_SI  = h_ox_feed * 0.0254;  % in to m
+
+  h_fuel_total = h_fuel_liquid + h_fuel_feed_SI; % m
+  h_ox_total  = h_ox_liquid + h_ox_feed_SI;   % m
+
+  g = 9.81; % m/s^2 at liftoff
+
+  dP_head_fuel_pa = params.fuel_density * g * h_fuel_total; % Pa
+  dP_head_ox_pa  = params.ox_density * g * h_ox_total;   % Pa
+
+  dP_head_fuel = dP_head_fuel_pa * 0.000145038; % Pa to psi
+  dP_head_ox  = dP_head_ox_pa * 0.000145038;  % Pa to psi
+
+ % find dP from heritage and add conservative overhead
+
    injector_dP = 0.2 * Prop.Pc; % 20% stiffness for low freq instability
 
    fuel_feed_dP = 20 + 20; % from Pandora flight system hotfire + conservative overhead (can be orificed) 
 
    ox_feed_dP = 14.4 + 20; % from Pandora flight system hotifre + conservative overhead (can be orificed) 
 
+   % find total dP across feed system
+
    fuel_dp = injector_dP + fuel_feed_dP;
 
    ox_dp = injector_dP + ox_feed_dP;
 
     % add dP to chamber pressure to find tank pressure
-    fuel_tank_press = fuel_dp + Prop.Pc; % psi
-    ox_tank_press   = ox_dp + Prop.Pc; % psi
+    fuel_tank_press = fuel_dp - dP_head_fuel + Prop.Pc; % psi
+    ox_tank_press   = ox_dp - dP_head_ox + Prop.Pc; % psi
 
     Press.tank_press = max(fuel_tank_press, ox_tank_press); % psi
 
     % Find He volume needed 
 
-    prop_mass_SI = Prop.prop_mass / 2.20462; % lbm to kg
-    fuel_mass_SI = prop_mass_SI / (Prop.OF + 1); % kg
-    ox_mass_SI   = prop_mass_SI - fuel_mass_SI; % kg
+    % find tank vollumes by using mass, density, and ullage
 
     fuel_tank_volume = (fuel_mass_SI / params.fuel_density) / (1 - params.ullage); % m^3
     ox_tank_volume   = (ox_mass_SI / params.ox_density) / (1 - params.ullage); % m^3
